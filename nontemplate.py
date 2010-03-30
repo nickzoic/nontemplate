31e0cf1742d412232d9f19c8ed33311f60d41b83
nick <nick@pluto.zoic.org>
2010-03-30 22:17:47 +1100

    Very almost cool

--- nontemplate.py
+++ nontemplate.py
@@ -1,95 +1,65 @@
 #!/usr/bin/env python
 
-class DocumentTag:
-    
-    name = None
-    attrs = {}
-    elems = []
+class Element:
     
     def __init__(self, document, name):
-        self.name = name
         self.document = document
-        self.tagged = False
+        self.parent = document._cursor
+        self.children = []
+        self.name = name
+        self.attrs = []
+        print "INIT %s" % self.name
         
-    def __call__(self, *args, **kwargs):
-        self.attr = kwargs
-        
-        for a in args:
-            if type(a) is dict:
-                self.attrs.update(a)
-            elif type(a) in (unicode, str):
-                self.elems.append(a)
-            elif type(a) in (list, tuple):
-                self.elems += str(a)
-            else:
-                self.elems += a.elems
-            
-        return self
-    
     def __enter__(self):
-        if not self.tagged:
-            self.document._tag(self.name)
-            self.tagged = True
-        self.document._rewind()
-            
-    def __exit__(self, type, value, tb):
-        self.document._forward()
+        print "ENTER %s" % self.name
+        self.document._cursor = self
+    
+    def __exit__(self, *stuff):
+        print "EXIT %s" % self.name
+        self.document._cursor = self.parent
+
+    def __call__(self, *args, **kwargs):
+        print "CALL %s" % self.name
+        self.attrs += [ (k[1:] if k[0] == '_' else k, v) for k, v in kwargs.iteritems() ]
+        self.children += args
+        return self
+        
+    def append(self, elem):
+        self.children.append(elem)
 
     def __str__(self):
-        return ( ("<%s" % self.name ) +
-            ''.join([ '%s="%s"' % x for x in self.attrs.iteritems() ]) +
-            ">" +
-            ''.join(self.elems) +
-            ("</%s>" % self.name)
-        )
+        xml =  ("<%s" % self.name) + (''.join( ' %s="%s"' % (k, v) for k, v in self.attrs))
+        if self.children:
+            xml += ">" + (''.join( [str(e) for e in self.children] )) + ("</%s>\n" % self.name)
+        else:
+            xml += "/>\n";
+        return xml
     
 class Document:
 
-    _head = []
-    _tail = []
+    _cursor = []
     
-    def _tag(self, tagname, *args, **kwargs):
-        opentag = "<%s" % tagname
-        for x,y in kwargs.iteritems():
-            opentag += ' %s="%s"' % ((x[1:] if x[0] == '_' else x), y)
-        opentag += ">"
-        self._head.append(opentag)
-        
-        for x in args:
-            if type(x) in (list, tuple):
-                self._head += list(x)
-            elif type(x) in (str, unicode):
-                self._head.append(x)
-            else:
-                x()
-                
-        self._head.append("</%s>\n" % tagname)
-    
-    def _text(self, text):
-        self._head += [ text ]
-        
-    def _rewind(self):
-        self._tail.append(self._head.pop())
-        
-    def _forward(self):
-        self._head.append(self._tail.pop())
-            
     def __getattr__(self, name):
-        return DocumentTag(self, name)
-
-    def __repr__(self):
-        return "Document Instance"
-
+        '''calling D.foo() causes this to be run ...'''
+        newelem = Element(self, name)
+        self._cursor.append(newelem)
+        return newelem
     
+    def __str__(self):
+        return "\n".join(str(e) for e in self._cursor)
+
+#----------------------------------------------------------------------------
+
 D = Document();
 
 with D.html:
-    D.head(D.title("foo"))
+    D.head(D.title("foo"), D.meta("something"))
+    D.neck("ha ha ha")
     with D.body:
         for y in range(0,3):
-            with D.p(_class="bar"):
+            with D.p(_class = "bar"):
                 for x in range(0,3):
                     D.img(src="baz%d-%d.jpg" % (x,y))
                     D.caption("Testing %d-%d" % (y,x))
             
-print ''.join(D._head)
+print str(D)
